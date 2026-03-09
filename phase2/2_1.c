b6c101b3940120675473d2848a8863dfb9123968
Nuhas <mdnaimurhasan99@gmail.com>
2026-03-09 08:12:17 +0600

    2_1.c is written

--- phase2/2_1.c
+++ phase2/2_1.c
@@ -1,2 +1,26 @@
 //Ask the user for a single integer. Print "Even" if it's divisible by 2, otherwise "Odd".
-#include
+#include <stdio.h>
+int main() {
+int a;
+printf("Enter an integer:");
+scanf("%d", &a);
+if (a % 2)
+{
+printf("'%d' is even Number",a);
+}
+else
+{
+    printf("'%d' is odd number",a);
+}
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
