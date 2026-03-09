e7725deb21b04aa1254a5459fb22b9dfc4f43816
Nuhas <mdnaimurhasan99@gmail.com>
2026-03-09 07:00:32 +0600

    error fixing modification

---
+++ phase1/1_2.c
@@ -0,0 +1,18 @@
+/*
+ * CHALLENGE: SWAP Meet
+ * Write a program that:
+ * 1. Takes two integers from the user.
+ * 2. Prints them before swap.
+ * 3. Swaps their values using a third temporary variable.
+ * 4. Prints them after swap.
+ */
+#include <stdio.h>
+
+int main() {
+int a, b, temp;
+printf("Enter integer a:");
+scanf("%d", &a);
+printf("Enter integer b:");
+scanf("%d", &b);
+    return 0;
+}
