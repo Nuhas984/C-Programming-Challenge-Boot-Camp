5fb93182b98b5a33620ba8eec793c946e712f9ce
Nuhas <mdnaimurhasan99@gmail.com>
2026-03-09 19:52:08 +0600

    3.2.c done

--- phase3/3_2.c
+++ phase3/3_2.c
@@ -1,8 +1,25 @@
+/*
+How many numbers? 4
+Enter number 1: 10
+Enter number 2: 20
+Enter number 3: 30
+Enter number 4: 40
+Sum = 100
+*/
 
 #include <stdio.h>
-
 int main()
 {
+    int count, sum = 0, a, number;
+    printf("how many numbers?\n");
+    scanf("%d", &count);
 
+    for (a = 1; a <= count; a++)
+    {
+        printf("Number %d :", a);
+        scanf("%d", &number);
+        sum = sum + number;
+    }
+    printf("The sum is %d", sum);
     return 0;
 }
