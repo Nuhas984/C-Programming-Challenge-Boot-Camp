31e59499df7e0a67bb8d1e935fc697f70fcee8b9
Nuhas <mdnaimurhasan99@gmail.com>
2026-03-09 10:17:53 +0600

    phase 2 done

--- phase2/2_3.c
+++ phase2/2_3.c
@@ -1,13 +1,24 @@
 // Leap year checker
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int year;
     printf("Enter a year: ");
     scanf("%d", &year);
     // printf("%d is ok", year);
-if(year % 400 == 0 || year % 4 == 0) {
-printf("%d This is a leap Year\n", year);
-}
+    if (year % 100 == 0 && year % 400 == 0)
+    {
+        printf("This is a leap year\n");
+    }
+    else if (year % 4 == 0 && year % 400 ==0)
+    {
+        printf("This is a leap Year\n", year);
+    }
+    else
+    {
+        printf("not a leap year\n");
+    }
+    
     return 0;
 }
