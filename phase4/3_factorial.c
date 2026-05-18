ad69d6a4ee3bf0e6429fa4894511e1fd1d4b43e8
Nuhas <mdnaimurhasan99@gmail.com>
2026-05-18 06:32:23 +0600

    factorial finished

--- phase4/3_factorial.c
+++ phase4/3_factorial.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
-int factorial(int n) {
-if(n == 0){
-return 1;
+int factorial(int n){
+    if (n == 0)
+    {
+        return 1;
+    }
+    return n * factorial(n-1);
 }
-return n *factorial(n-1);
-}
-
-
 int main() {
-    
-
+int number;
+scanf("%d",&number);
+if (number<0)
+{
+printf("Factorial is not defined");
+}
+else{
+    int result = factorial(number);
+    printf("%d! = %d",number,result);
+}
     return 0;
 }
